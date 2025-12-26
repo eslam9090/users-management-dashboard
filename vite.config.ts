824195f354dac66c7c483e82e10250a687ad7a0b
eslam9090 <eslammyaakoup7090@gmail.com>
2025-12-26 22:51:54 +0200

    edite-packagejason

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/users-management-dashboard/', // Add this line
+  build: {
+    outDir: 'dist'
+  }
 })
